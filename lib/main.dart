d16e776900af8bdaa0450983907f51b666d82659
Alyona Chebanyk <alyonachebanyk@gmail.com>
2022-10-13 19:37:09 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      Container(
+        alignment: Alignment.center,
+        child: Text(
+        'Hello World!',
+        textDirection: TextDirection.ltr,
+        textAlign: TextAlign.center, 
+          textScaleFactor: 2.0,
+    ),
+      )
+  );
+}
